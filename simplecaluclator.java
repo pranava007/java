2cdc2c7472054a4870f2b538fb1bc0513bd79c1e
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 10:32:10 +0530

    java

--- simplecaluclator.java
+++ simplecaluclator.java
@@ -4,10 +4,10 @@ class simplecaluclator
     public static void main(String[]arge)
     {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter a value" + int a=scanner.nextInt() );
-       
-        System.out.println("Enter b value" +  int b=scanner.nextInt());
-       
+        System.out.println("Enter a value");
+        int a=scanner.nextInt();
+        System.out.println("Enter b value");
+        int b=scanner.nextInt();
         int sum=a+b;
     
         System.out.println("Your anwers :"+ sum);
