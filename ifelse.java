7355884262adbf7f8750a71f480d36b9817e7007
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 12:47:09 +0530

    java

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+public class ifelse 
+{
+    public static void main(String[]arge)
+    {
+        int a=10;
+        if(a==20)
+        {
+         System.out.println("done");
+        }
+        else
+        {
+         System.out.println("worng");
+        }
+    }
+    
+}
