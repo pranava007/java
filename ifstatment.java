519c39cdc08e67b65d110d0363276b0a7002d10b
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 12:40:50 +0530

    java

---
+++ ifstatment.java
@@ -0,0 +1,12 @@
+public class ifstatment
+{
+    public static void main(String[]arge)
+    {
+        int a=10;
+        if(a==10)
+        {
+            System.out.println("cort");
+        }
+    }
+    
+}
