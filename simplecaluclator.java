17727ea603a61e68506ab6c580d9e0427d7e8ae4
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 09:41:57 +0530

    java

---
+++ simplecaluclator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class simplecaluclator
+{
+    public static void main(String[]arge)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a value" + int a=scanner.nextInt() );
+       
+        System.out.println("Enter b value" +  int b=scanner.nextInt());
+       
+        int sum=a+b;
+    
+        System.out.println("Your anwers :"+ sum);
+    }
+   
+    
+
+}
