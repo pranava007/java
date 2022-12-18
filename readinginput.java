25b5c557e0d6661201f54c1c7e59c3714c789b13
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 09:19:46 +0530

    java

---
+++ readinginput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class readingiput
+{
+public static void main(String[]arge)
+{
+    Scanner scanner = new Scanner(System.in);
+    int age = scanner.nextInt();
+    System.out.println("Your age:"+age);
+    
+}
+}
