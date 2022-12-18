b725dcfe6a18dc380ec8e790b83ad1ca9b9957ad
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 12:54:28 +0530

    java

---
+++ elseif.java
@@ -0,0 +1,22 @@
+public class elseif
+{
+public static void main(String[]arge)
+{
+    int a=11;
+    if(a<11)
+    {
+       System.out.println("good");
+    }
+    else if(a!=11)
+    {
+        System.out.println("bad");
+    }
+    else
+    {
+        System.out.println("worng");
+    }
+}
+
+    
+}
+
