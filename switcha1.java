3d8cdc708725e7b094577ce8cf6535746032c070
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 22:33:26 +0530

    java

---
+++ switcha1.java
@@ -0,0 +1,42 @@
+ class switcha1
+{
+    public static void main(String[]arge)
+    
+    
+    {
+        int a=1;
+
+        switch (a) {
+            case 1:
+    
+            System.out.println("one");
+                
+                break;
+    
+                case 2:
+    
+                System.out.println("two");
+                
+                break;
+    
+                case 3:
+    
+                  System.out.println("three");
+                break;
+    
+        
+                default:
+    
+                System.out.println("othe number");
+    
+                break;
+        }
+
+
+
+
+
+    }
+   
+    
+}
