7e33d7bf6bd9afb5d472cc45f629b343f625cd73
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-18 08:20:40 +0530

    java

---
+++ datatypes.java
@@ -0,0 +1,41 @@
+class datatypes
+{
+        public static void main(String[]arge)
+        {
+                int age=22;
+                char name='#';
+                double decimal = 35.6;
+                String nam ="pranavamuthu";
+                boolean sum = true;
+                
+
+                System.out.println(age);
+                System.out.println(name);
+                System.out.println(decimal);
+                System.out.println(nam);
+                System.out.println(sum);
+        }
+}
+
+
+//char => single letter or sysbel or number just print
+
+
+/*
+primitive 
+
+boolean =>boolean
+numeric => character => char
+        => intagral =>/ integer =>#floating_point
+                            
+        =>/int,byte,short,long
+        =>#fload,double
+
+noprimitive
+string
+array
+etc.....
+
+
+
+ */
