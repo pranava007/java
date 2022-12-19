560ca5c5100f7f05a8a019de8b4050a86599eb15
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-19 15:17:09 +0530

    java

---
+++ forloop.java
@@ -0,0 +1,12 @@
+class forloop 
+{
+    public static void main(String[]arge)
+    {
+       // int sum=1;
+        for(int sum=1; sum<=10; sum++)
+        {
+            System.out.println(sum);
+        }
+    }
+    
+}
