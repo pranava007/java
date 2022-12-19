621d4e5290a9298c8b42f11c90ad6839e5d04e3a
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-19 15:32:55 +0530

    java

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+public class dowhile
+{
+    public static void main(String[]arge) 
+    {
+        int num=1;
+        do
+        {
+            
+            System.out.println(num);
+            num++;
+        }while(num <= 0);
+    }
+    
+}
