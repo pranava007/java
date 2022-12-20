5b389eaba6341674b104a2e10c501bcf566a6ecb
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-20 13:41:55 +0530

    java

---
+++ Logicaloperater.java
@@ -0,0 +1,10 @@
+public class Logicaloperater 
+{
+    public static void main(String arge[])
+    {
+    int a=55,b=85;
+    System.out.println("&& and operater :"+(a>35 && b>38));
+    System.out.println("|| or operater :"+ (a>88 || b>88));
+    }
+    
+}
