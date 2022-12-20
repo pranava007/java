8c3e05b0cfa0fdf75a9c66164e778a3a73f3f89f
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-20 13:23:29 +0530

    java

---
+++ relationopperator.java
@@ -0,0 +1,16 @@
+public class relationopperator 
+{
+    public static void main (String arge[])
+    {
+        int a=10,b=22;
+        System.out.println("equal to           :"+(a==b));
+        System.out.println("Not equal to       :"+(a!=b));
+        System.out.println("grater then        :"+(a>b));
+        System.out.println("less then          :"+(a<b));
+        System.out.println("grater then equal  :"+(a>=b));
+        System.out.println("less then equal to :"+(a<=b));
+
+    }
+
+    
+}
