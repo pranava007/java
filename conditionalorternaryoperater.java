9b40956035823bdfd2b55c1b2e44f6e4babdad3d
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-20 13:50:52 +0530

    conditional or Ternary operater

---
+++ conditionalorternaryoperater.java
@@ -0,0 +1,10 @@
+public class conditionalorternaryoperater 
+{
+    public static void main(String arge[])
+    {
+        int a=35,b=85,c;
+        c=a>b?a:b;
+        System.out.println("gread number :" +c);
+    }
+    
+}
