95841c2b2c9b8b8d0799b283e14d087de7050347
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-20 11:36:54 +0530

    test

---
+++ arithimatiall.java
@@ -0,0 +1,14 @@
+class arithimatiall 
+{
+    public static void main(String arge []) 
+    {
+        int a=10,b=200;
+        System.out.println("add       :"+(a+b));
+        System.out.println("sub       :"+(a-b));
+        System.out.println("multi     :"+(a*b));
+        System.out.println("devition  :"+(a/b));
+        System.out.println("modules   :"+(a%b));
+
+        
+    }
+}
