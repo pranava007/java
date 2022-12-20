b5f41d075ad9b87c2ffaa5f7f263b93f2ee3d3b6
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-20 11:18:31 +0530

    java

---
+++ casting.java
@@ -0,0 +1,13 @@
+class casting 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		double b=a,d=25.5555;
+		int c=(int)d;
+		System.out.println("int :"+a);
+		System.out.println("double :"+b);
+        System.out.println("c :"+c);
+		System.out.println("d :"+d);
+	}
+}
