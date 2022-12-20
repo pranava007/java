6aab00bdb150d971449da8bbacbed730874da3a7
pranava muthu <103396008+pranava007@users.noreply.github.com>
2022-12-20 12:03:59 +0530

    java

---
+++ assigmentoperator.java
@@ -0,0 +1,21 @@
+public class assigmentoperator 
+{
+    public static void main(String arge[])
+    {
+        int a=10;
+        System.out.println(a);
+        a +=10;
+        System.out.println(a);
+        a -=10;
+        System.out.println(a);
+        a *=10;
+        System.out.println(a);
+        a /=10;
+        System.out.println(a);
+        a %=10;
+        System.out.println(a);
+        
+        
+
+    }    
+}
